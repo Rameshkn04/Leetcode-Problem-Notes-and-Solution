50b13f21515dd8176f45c08309e90e403356caad
Rameshkn04 <rameshkn04052004@gmail.com>
2026-01-12 22:40:23 +0530

    Implement countNodes for complete binary tree

---
+++ solutions/BinarySearch/222. Count Complete Tree Nodes.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+
+        def leftHeight(node):
+            h = 0
+            while node:
+                h += 1
+                node = node.left
+            return h
+
+        def rightHeight(node):
+            h = 0
+            while node:
+                h += 1
+                node = node.right
+            return h
+
+        lh = leftHeight(root)
+        rh = rightHeight(root)
+
+        # Perfect binary tree
+        if lh == rh:
+            return (1 << lh) - 1
+
+        # Otherwise recurse
+        return 1 + self.countNodes(root.left) + self.countNodes(root.right)
