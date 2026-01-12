f15d57e0c701e633d06e90a18691c06ad458e188
Rameshkn04 <rameshkn04052004@gmail.com>
2026-01-12 20:38:59 +0530

    Add solution for counting islands in a grid
    Implement depth-first search to count islands in a grid.
    Important graph question

---
+++ solutions/graphs/200.Number_of_island.py
@@ -0,0 +1,27 @@
+class Solution:
+  def graph(self, grid):
+    if not grid:
+      return 0
+    rows,cols = len(grid), len(grid[0])
+    visited = set()
+    island = 0
+
+    def dfs(r,c):
+      if (r < 0 or r >= rows or c < 0 or c >= cols or grid[r][c] == '0' or (r,c) in visited):
+        return
+      visited.add((r,c))
+      #visit all four neighbours
+      dfs(r+1,c) #down
+      dfs(r-1,c) #up
+      dfs(r,c+1) #right
+      dfs(r,c-1) #left
+    #Traverse the grid
+    for r in range(rows):
+      for c in range(cols):
+        if grid[r][c] == '1' and (r,c) not in visited:
+          dfs(r,c)
+          island += 1
+    return island
+
+
+#Most important queston it was asked in Snabbit Exam must practise this question daily 
