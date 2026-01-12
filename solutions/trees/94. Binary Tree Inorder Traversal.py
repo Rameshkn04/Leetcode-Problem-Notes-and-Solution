8814a97c816a75dee775de4125bb251c603eea05
Rameshkn04 <rameshkn04052004@gmail.com>
2026-01-12 20:46:48 +0530

    Implement inorder traversal for binary tree

---
+++ solutions/trees/94. Binary Tree Inorder Traversal.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def inorderTraversal(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        result = []
+        def dfs(node):
+          if not node:
+            return
+          dfs(node.left):
+          result.append(node.val)
+          dfs(node.right)
+        dfs(root)
+        return result
+        
