da43e6b8870c7279c021f6c484bb74dd7b73e55f
Rameshkn04 <rameshkn04052004@gmail.com>
2026-01-13 22:39:54 +0530

    Implement findDuplicate method using Floyd's Tortoise

---
+++ solutions/BinarySearch/287. Find the Duplicate Number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow = fast = nums[0]
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+            if slow == fast:
+                break
+        #Phase 2 Find the duplicate
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+        return slow
