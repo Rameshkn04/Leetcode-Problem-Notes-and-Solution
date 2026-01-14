ddb26c3387d5f827560377eeb2f095fa07296334
Rameshkn04 <rameshkn04052004@gmail.com>
2026-01-14 22:00:54 +0530

    Create 350. Intersection of Two Arrays II.py

---
+++ solutions/BinarySearch/350. Intersection of Two Arrays II.py
@@ -0,0 +1,13 @@
+from collections import Counter
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        
+        count = Counter(nums1)
+        result = []
+        for num in nums2:
+            if count[num] > 0:
+                result.append(num)
+                count[num] -= 1
+        return result
