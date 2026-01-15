ead9f226eca0b3dbb846e41941b6132b55e67f7b
Rameshkn04 <rameshkn04052004@gmail.com>
2026-01-15 23:29:46 +0530

    Create 152. Maximum Product Subarray.py

---
+++ solutions/dp/152. Maximum Product Subarray.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        curr_max = nums[0]
+        curr_min = nums[0]
+        result = nums[0]
+
+        for i in range(1,len(nums)):
+            num = nums[i]
+            if num < 0:
+                curr_max,curr_min = curr_min,curr_max
+
+            curr_max = max(num, num*curr_max)
+            curr_min = min(num, num*curr_min)
+
+            result = max(result, curr_max)
+        return result
