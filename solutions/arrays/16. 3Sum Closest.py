a6486034a75c7e8c7b630a7dd1747e46297a4c3c
Rameshkn04 <rameshkn04052004@gmail.com>
2026-01-16 22:21:14 +0530

    Update 16. 3Sum Closest.py

--- solutions/arrays/16. 3Sum Closest.py
+++ solutions/arrays/16. 3Sum Closest.py
@@ -16,3 +16,15 @@ class Solution:
                 else:
                     return target
         return closest
+'''
+Example 1:
+
+Input: nums = [-1,2,1,-4], target = 1
+Output: 2
+Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+Example 2:
+
+Input: nums = [0,0,0], target = 1
+Output: 0
+Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+'''
