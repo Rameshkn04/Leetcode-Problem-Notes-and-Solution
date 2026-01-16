197a81a35b311339ba55fca33bf4b2e72b7482da
Rameshkn04 <rameshkn04052004@gmail.com>
2026-01-16 22:26:01 +0530

    Update 34. Find First and Last Position of Element in Sorted Array.py

--- solutions/arrays/34. Find First and Last Position of Element in Sorted Array.py
+++ solutions/arrays/34. Find First and Last Position of Element in Sorted Array.py
@@ -32,3 +32,17 @@ class Solution:
         right_index = find_right(nums,target)
         return [left_index, right_index]
 
+'''
+Example 1:
+
+Input: nums = [5,7,7,8,8,10], target = 8
+Output: [3,4]
+Example 2:
+
+Input: nums = [5,7,7,8,8,10], target = 6
+Output: [-1,-1]
+Example 3:
+
+Input: nums = [], target = 0
+Output: [-1,-1]
+'''
