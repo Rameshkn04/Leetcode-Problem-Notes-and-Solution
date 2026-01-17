d56bc7493d09bb115e1479f5afdddcdf78224175
Rameshkn04 <rameshkn04052004@gmail.com>
2026-01-17 21:45:21 +0530

    Update 35. Search Insert Position.py

--- solutions/BinarySearch/35. Search Insert Position.py
+++ solutions/BinarySearch/35. Search Insert Position.py
@@ -10,3 +10,17 @@ class Solution:
             else:
                 right = mid - 1
         return left
+'''
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+'''
