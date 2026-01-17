279b710999554af0b4365996b9b5985332801746
Rameshkn04 <rameshkn04052004@gmail.com>
2026-01-17 21:41:13 +0530

    Update 51. N-Queens.py

--- solutions/arrays/51. N-Queens.py
+++ solutions/arrays/51. N-Queens.py
@@ -25,3 +25,14 @@ class Solution:
                 diag2.remove(row+col)
         backtrack(0)
         return res
+'''
+Example 1:
+
+Input: n = 4
+Output: [[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+Explanation: There exist two distinct solutions to the 4-queens puzzle as shown above
+Example 2:
+
+Input: n = 1
+Output: [["Q"]]
+'''
