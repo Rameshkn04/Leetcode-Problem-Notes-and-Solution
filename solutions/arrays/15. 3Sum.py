969a5a320ee7b1c247cc54df2d30425f53fc051d
Ramesh K N <rameshkn04052004@gmail.com>
2026-01-25 21:47:12 +0530

    Update 15. 3Sum.py

--- solutions/arrays/15. 3Sum.py
+++ solutions/arrays/15. 3Sum.py
@@ -21,7 +21,6 @@ class Solution:
                         l += 1
                     while l < r and nums[r] == nums[r-1]:
                         r -= 1
-
                     l += 1
                     r -= 1
         return res 
