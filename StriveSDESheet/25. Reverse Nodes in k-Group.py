dc7c759d725895adf896dca2baef4afdeebeed60
Ramesh K N <rameshkn04052004@gmail.com>
2026-01-26 21:53:49 +0530

    Create 25. Reverse Nodes in k-Group.py

---
+++ StriveSDESheet/25. Reverse Nodes in k-Group.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        count = 0
+        node = head
+        while node and count < k:
+            node = node.next
+            count += 1
+        
+        if count == k:
+            prev = self.reverseKGroup(node,k)
+            current = head
+            while count > 0:
+                temp = current.next
+                current.next = prev
+                prev = current
+                current = temp
+                count -= 1
+            return prev
+        return head
